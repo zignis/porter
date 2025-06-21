f077fa021444dcf479c56dccbd34a90a8c80bef9
zignis <velociraptorm77@gmail.com>
2025-06-21 11:03:12 +0530

    generate release

---
+++ Porter/Views/UpdaterView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdaterView.swift
+//  Porter
+//
+//  Created by zignis on 19/06/25.
+//
+
+import SwiftUI
+
+struct UpdaterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdaterView()
+}
